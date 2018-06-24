fba362fc8ffbf773689165507da14fcc8663cd39
XiaoYun <156081283@qq.com>
2018-06-24 23:13:32 +0800

    俱乐部页面（6-24）

--- src/business/club/createClub.vue
+++ src/business/club/createClub.vue
@@ -1,257 +1,170 @@
 <template>
     <div class="create-club">
-        <div class="select-upload">
-            <div class="upload-box text-center" >
+        <l-head>
+            <l-icon name="fanhui" @click.native="$router.push('club')" slot="left-item"/>
+             创建俱乐部
+        </l-head>
+        <div class="select-upload" @click="toHome">
+            <div class="upload-box text-center">
                 <l-icon name="shangchuantouxiang"/>
-                <l-imageUpload   :limit="1"
-                                 :action="'http://api.bozhiyue.com/my/uploadimg'"
-                                 style="border-radius: 50%;" :onSuccess="uploadHeadSuccess"/>
-            </div>
-            <div class="select-upload-label text-center">上传俱乐部头像</div>
+                </div>
+                <div class="select-upload-label text-center">上传俱乐部头像</div>
         </div>
         <div class="club-item mt-20">
             <div class="left-label pull-left">名称</div>
             <div class="right-input pull-left">
-                <input type="text" placeholder="请输入俱乐部名称" v-model="formData.name" v-verify="formData.name">
+                <input type="text" placeholder="请输入俱乐部名称">
             </div>
         </div>
         <div class="club-item">
             <div class="left-label pull-left">地区</div>
             <div class="right-input pull-left">
-                <input type="text" placeholder="俱乐部位置" v-model="area" @click="selCity" readonly>
+                <input type="text" placeholder="请输入地区名称(xx市xx区)">
             </div>
         </div>
         <div class="club-item text-area">
             <div class="left-label pull-left">简介</div>
             <div class="right-input pull-left">
-                <textarea placeholder="在此输入俱乐部简介" v-model="formData.intro"></textarea>
+                <textarea placeholder="在此输入俱乐部简介"></textarea>
             </div>
         </div>
-
-        <div class="save-btn text-center" @click="submitData">提交</div>
-
+        <div class="select-pic text-center" @click="toHome">
+                <div class="upload-box">
+                  <div class="upload-pic">
+                   <l-icon name="shangchuantupian"/>
+                  </div>
+                <div class="select-upload-label">上传图片 0/1</div>
+                </div>
+        </div>
+        <div class="save-btn text-center" @click="createClubSuccess">提交</div>
     </div>
 </template>
+
 <script>
-    import citys from '../../api/citys.js'
-    import club from '../../api/club.js'
-    let _this
+let _this
     export default {
         name: 'create-club',
-        data() {
-            var memberid = this.session.getMemberID();
-            return {
-                formData: {
-                    memberid: memberid,
-                    name: "",
-                    proid: "",
-                    cityid: "",
-                    areaid: "",
-                    intro: "",
-                    x: "",
-                    y: "",
-                    logo: ""
-                },
-                area: "",
-                locationData: null,//定位数据
-                selectorJSON: null
-            }
-        }, verify: {
-            formData: {
-                name: [{minLength: 1, message: "俱乐部名称必须填写"}]
+        methods: {
+            reset() {
+                console.log(this.filterName)
+            },
+            createClubSuccess() {
+                _this.$router.push('createClubSuccess')
             }
         },
         mounted() {
             _this = this
-            _this.loadCity();
-            if(this.session.isAPPRuntime()){
-                api.addEventListener({
-                    name:'clip_success'
-                }, function(ret, err){
-                    if (ret) {
-                        var base64 = ret.value.base64;
-                        var content = ret.value.content;
-                        if(content=="head"){
-                            _this.head_pic = base64;
-                        }else{
-                            _this.intor_pic = base64;
-                        }
-                        //上传图片
-                        _this.appUtil.imageUpload(base64,function(json){
-
-                        });
-                    } else {
-                        alert(JSON.stringify(err));
-                    }
-                });
-            }
-        },activated(){
-            if (_this.session.isAPPRuntime() && this.area == '') {
-                _this.location();//定位
-            }
-        },
-        methods: {
-            submitData(){
-                if (_this.formData.proid == "" || _this.formData.cityid == "" || _this.formData.areaid == "") {
-                    _this.$Message.error("请选择俱乐部位置");
-                    return;
-                }
-                if(!_this.$verify.check()){
-                    var errMsg = this.appUtil.toastRemind(this.$verify.verifyQueue,this.$verify.$errors);
-                    _this.$Message.error(errMsg);
-                }else {
-                    club.clubCreate(_this, _this.formData);
-                }
-            },
-            selCity(){
-                if (_this.selectorJSON == null) {
-                    _this.$Message.info("正在加载城市数据...");
-                    return;
-                }
-                citys.actionSelector(_this.selectorJSON, function (ret) {
-                    _this.area = ret.level1 + " " + ret.level2 + " " + ret.level3;
-                    _this.formData.proid = ret.selectedInfo[0].id;
-                    _this.formData.cityid = ret.selectedInfo[1].id;
-                    _this.formData.areaid = ret.selectedInfo[2].id;
-                });
-            },
-            loadCity(){
-                citys.bulidJSONCity(function (json) {
-                    if (json.code == 1) {
-                        _this.selectorJSON = json.data;
-                    } else {
-                        _this.$Message.error("加载城市数据失败");
-                    }
-                });
-            },
-            location(){
-                citys.location(function (ret) {
-                    if (!ret.status) {
-                        _this.$Message.error("定位失败,请开启GPS");
-                        return;
-                    }
-                    _this.formData.x = ret.lon;
-                    _this.formData.y = ret.lat;
-                    _this.locationData = ret;
-                    var state = ret.state;
-                    var city = ret.city;
-                    var district = ret.district;
-                    var street = ret.street;
-                    var address = ret.address;
-                    var thoroughfare = ret.thoroughfare;
-                    _this.area = state + " " + city + " " + district;
-                    //匹配城市ID
-                    var dbData = citys.locationToDBData(_this, _this.locationData);
-                    if (dbData != null) {
-                        _this.formData.proid = dbData.proid;
-                        _this.formData.cityid = dbData.cityid;
-                        _this.formData.areaid = dbData.areaid;
-                    }
-                }, true);
-            },
-            uploadHeadSuccess(res) {
-                this.formData.logo = res;
-            }
         }
     }
 </script>
 
 <style lang="scss">
     .create-club {
-    .select-upload {
+        .select-upload {
         width:100%;
         background-color:#33333a;
         padding:50px 0;
         margin-bottom:40px;
-    .upload-box{
-        background-color:#25252B;
-        width:140px;
-        height:140px;
-        margin:0 auto 0;
-        border-radius:50%;
-        display: flex;
-        justify-content:center;
-        position: relative;
-        align-items:Center;
-
-    .icons {
-        width: 50px;
-        height: 44px;
-        color: #999999;
-    }
-    img {
-        width: 100%;
-        height: 100%;
-        border-radius:50%;
-    }
-    }
-    .select-upload-label {
-        color: #999999;
-        font-size: 30px;
-        line-height: 30px;
-        margin-top:20px;
-    }
-    }
-    .select-pic {
+        .upload-box{ 
+            background-color:#25252B;
+            width:140px;
+            height:140px;
+            margin:0 auto 0;
+            border-radius:50%;
+            display: flex;
+            justify-content:center;
+            align-items:Center;
+            .icons {
+            width: 50px;
+            height: 44px;
+            color: #999999;
+             }
+        }
+        .select-upload-label {
+            color: #999999;
+            font-size: 30px;
+            line-height: 30px;
+            margin-top:20px;
+            }
+     }
+     .select-pic {
         margin-bottom:0px;
         background-color: #33333a;
         padding:30px 0 140px 30px;
-    }
-    padding-bottom: 60px;
-    .club-item {
-        width: 750px;
-        overflow: hidden;
-        margin-bottom: 0px;
-        background-color: #33333a;
-        height: 90px;
-    .left-label {
-        width: 100px;
-        font-size: 30px;
-        line-height: 30px;
-        margin:30px 0 30px 30px;
-    }
-    .right-input {
-        width: 590px;
-        margin-top: 10px;
-    input {
-        width: 590px;
-        height: 70px;
-        background-color: #25252B;
-        color: #ffffff;
-        padding: 0 20px;
-        -webkit-border-radius: 10px;
-        -moz-border-radius: 10px;
-        border-radius: 10px;
-    }
-    }
-    &.text-area {
-         height: inherit;
-         padding: 0 0 10px 0;
-    textarea {
-        width: 590px;
-        height: 400px;
-        background-color: #25252B;
-        color: #ffffff;
-        padding: 10px 20px;
-        -webkit-border-radius: 10px;
-        -moz-border-radius: 10px;
-        border-radius: 10px;
-    }
-    }
-    }
-    .save-btn {
-        font-size: 34px;
-        line-height: 34px;
-        padding: 33px 0;
-        background-color: #F8C513;
-        color: #000;
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        width: 750px;
-    }
-    .mt-20 {
-        margin-top: -20px;
-    }
+        .upload-box{ 
+            background-color:#25252B;
+            width:210px;
+            height:210px;
+            border-radius:10px;
+            .icons {
+            width: 60px;
+            height: 52px;
+            padding-top:60px;
+            color: #999999;
+             }
+        }
+        .select-upload-label {
+            color: #999999;
+            font-size: 24px;
+            line-height: 15px;
+            margin-top:20px;
+            }
+     }
+        padding-bottom: 60px;
+        .club-item {
+            width: 750px;
+            overflow: hidden;
+            margin-bottom: 0px;
+            background-color: #33333a;
+            height: 90px;
+            .left-label {
+                width: 100px;
+                font-size: 30px;
+                line-height: 30px;
+                margin:30px 0 30px 30px;
+            }
+            .right-input {
+                width: 590px;
+                margin-top: 10px;
+                input {
+                    width: 590px;
+                    height: 70px;
+                    background-color: #25252B;
+                    color: #ffffff;
+                    padding: 0 20px;
+                    -webkit-border-radius: 10px;
+                    -moz-border-radius: 10px;
+                    border-radius: 10px;
+                }
+            }
+            &.text-area {
+                height: inherit;
+                padding: 0 0 10px 0;
+                textarea {
+                    width: 590px;
+                    height: 400px;
+                    background-color: #25252B;
+                    color: #ffffff;
+                    padding: 10px 20px;
+                    -webkit-border-radius: 10px;
+                    -moz-border-radius: 10px;
+                    border-radius: 10px;
+                }
+            }
+        }
+        .save-btn {
+            font-size: 34px;
+            line-height: 34px;
+            padding: 33px 0;
+            background-color: #F8C513;
+            color: #000;
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 750px;
+        }
+        .mt-20 {
+            margin-top: -20px;
+        }
     }
 </style>
